702b20f66a0df46620c84e78fc7745ebf16d3564
Kirill Tarasov <kir_tarasov@mail.ru>
2023-08-01 10:14:37 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        return (Math.abs(n) + '').split("").reverse().join("");
+    }
+    return (n + '').split("").reverse().join("");
 }
